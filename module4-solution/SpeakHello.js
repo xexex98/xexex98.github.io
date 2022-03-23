ff253e6e7438f835527f80babdc34fffa0889dd5
Morne Barra <mbarra@si-p4.kzn.21-school.ru>
2022-03-23 21:51:51 +0300

    solution

---
+++ module4-solution/SpeakHello.js
@@ -0,0 +1,8 @@
+(function (window) {
+  var helloSpeaker = {};
+  var speakWord = "Hello";
+  helloSpeaker.speak = function speak(name) {
+    console.log(speakWord + " " + name);
+  }
+  window.helloSpeaker = helloSpeaker;
+})(window);
