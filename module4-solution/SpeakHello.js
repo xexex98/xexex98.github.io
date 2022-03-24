2edb2a0764f8f89cbbbe0b1f89437fb6c8730820
Morne Barra <mbarra@ev-s3.kzn.21-school.ru>
2022-03-24 20:11:12 +0300

    5

--- module4-solution/SpeakHello.js
+++
@@ -1,8 +0,0 @@
-(function (window) {
-  var helloSpeaker = {};
-  var speakWord = "Hello";
-  helloSpeaker.speak = function speak(name) {
-    console.log(speakWord + " " + name);
-  }
-  window.helloSpeaker = helloSpeaker;
-})(window);
